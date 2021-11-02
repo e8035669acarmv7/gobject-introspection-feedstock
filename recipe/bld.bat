81b52aa1cd04144419521cf5a82835cc44437726
Tom Schoonjans <Tom.Schoonjans@genomicsplc.com>
2021-11-02 21:13:40 +0000

    Fix option name

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -30,7 +30,7 @@ set "PYTHONIOENCODING=UTF-8"
 @REM fix the env python path to match).
 FOR /F "delims=" %%i IN ('cygpath.exe -m "%PYTHON%"') DO set "PYTHON_M=%%i"
 
-%BUILD_PREFIX%\Scripts\meson.exe --buildtype=release --prefix=%LIBRARY_PREFIX_M% --wrap-mode=nofallback --backend=ninja -Dcairo=enabled -Dcairo-libname=cairo-gobject.dll -Dpython=%PYTHON_M% ..
+%BUILD_PREFIX%\Scripts\meson.exe --buildtype=release --prefix=%LIBRARY_PREFIX_M% --wrap-mode=nofallback --backend=ninja -Dcairo=enabled -Dcairo_libname=cairo-gobject.dll -Dpython=%PYTHON_M% ..
 if errorlevel 1 exit 1
 
 ninja -v
